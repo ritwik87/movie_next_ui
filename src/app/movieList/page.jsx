065c5c8600166f30a24c1d65bbb7f80191ad6115
Ritwik Mukhopadhyay <ritwikmukhopadhyay@gmail.com>
2024-09-10 16:34:05 +0530

    Semantic HTML changes

--- src/app/movieList/page.jsx
+++ src/app/movieList/page.jsx
@@ -15,39 +15,41 @@ const PaginationComponent = ({ currentPage, totalPages, onPageChange }) => {
   const { t } = useTranslation();
 
   return (
-    <Pagination className="justify-content-center mt-4">
-      <Pagination.Prev
-        onClick={() => onPageChange(currentPage - 1)}
-        disabled={currentPage === 1}
-        as="span"
-        className="nav-button"
-      >
-        <span style={{ color: currentPage === 1 ? "grey" : "" }}>
-          {t("Prev")}
-        </span>
-      </Pagination.Prev>
-
-      {[...Array(totalPages)].map((_, index) => (
-        <Pagination.Item
-          key={index + 1}
-          active={index + 1 === currentPage}
-          onClick={() => onPageChange(index + 1)}
+    <nav aria-label="Movies Pagination">
+      <Pagination className="justify-content-center mt-4">
+        <Pagination.Prev
+          onClick={() => onPageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          as="span"
+          className="nav-button"
         >
-          {index + 1}
-        </Pagination.Item>
-      ))}
+          <span style={{ color: currentPage === 1 ? "grey" : "" }}>
+            {t("Prev")}
+          </span>
+        </Pagination.Prev>
 
-      <Pagination.Next
-        onClick={() => onPageChange(currentPage + 1)}
-        disabled={currentPage === totalPages}
-        as="span"
-        className="nav-button"
-      >
-        <span style={{ color: currentPage === totalPages ? "grey" : "" }}>
-          {t("Next")}
-        </span>
-      </Pagination.Next>
-    </Pagination>
+        {[...Array(totalPages)].map((_, index) => (
+          <Pagination.Item
+            key={index + 1}
+            active={index + 1 === currentPage}
+            onClick={() => onPageChange(index + 1)}
+          >
+            {index + 1}
+          </Pagination.Item>
+        ))}
+
+        <Pagination.Next
+          onClick={() => onPageChange(currentPage + 1)}
+          disabled={currentPage === totalPages}
+          as="span"
+          className="nav-button"
+        >
+          <span style={{ color: currentPage === totalPages ? "grey" : "" }}>
+            {t("Next")}
+          </span>
+        </Pagination.Next>
+      </Pagination>
+    </nav>
   );
 };
 
@@ -88,91 +90,99 @@ const MovieList = () => {
 
   if (isLoading || isFetching) {
     return (
-      <Container
-        className="d-flex justify-content-center align-items-center"
-        style={{ height: "100vh" }}
-      >
-        <Spinner animation="border" role="status">
-          <span className="visually-hidden">{t("Loading...")}</span>
-        </Spinner>
-      </Container>
+      <main>
+        <section
+          aria-label="Loading Movies"
+          className="d-flex justify-content-center align-items-center"
+          style={{ height: "100vh" }}
+        >
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">{t("Loading...")}</span>
+          </Spinner>
+        </section>
+      </main>
     );
   }
 
   if (error) {
     console.error("Error fetching movies:", error);
-    return <div>{t("Error loading movies. Please try again later.")}</div>;
+    return <main>{t("Error loading movies. Please try again later.")}</main>;
   }
 
   const totalMovies = moviesData?.length || 0;
   const totalPages = Math.ceil(totalMovies / params.pageSize);
 
   return (
-    <Container className="my-5 position-relative z-3">
-      <div className="d-flex justify-content-between">
-        <PageHeadingComponent
-          title={t("My Movies")}
-          Element={
-            <PlusCircle
-              style={{ cursor: "pointer" }}
-              color="#fff"
-              className="fs-6 pe-auto"
-              onClick={() => router.push("/movieList/modifyMovie/0")}
-            />
-          }
-        />
+    <main>
+      <Container className="my-5 position-relative z-3">
+        <header className="d-flex justify-content-between">
+          <PageHeadingComponent
+            title={t("My Movies")}
+            Element={
+              <PlusCircle
+                style={{ cursor: "pointer" }}
+                color="#fff"
+                className="fs-6 pe-auto"
+                onClick={() => router.push("/movieList/modifyMovie/0")}
+              />
+            }
+          />
 
-        <button
-          className="btn text-white py-4 d-flex align-items-center gap-2"
-          style={{
-            cursor: "pointer",
-            background: "none",
-            border: "none",
-            padding: 0,
-            outline: "none",
-            boxShadow: "none",
-            marginTop: "5px",
-          }}
-          onClick={handleLogout}
-        >
-          <span className="d-none d-md-inline">{t("Log Out")}</span>
-          <BoxArrowRight color="#fff" className="fs-6 pe-auto" />
-        </button>
-      </div>
+          <button
+            className="btn text-white py-4 d-flex align-items-center gap-2"
+            style={{
+              cursor: "pointer",
+              background: "none",
+              border: "none",
+              padding: 0,
+              outline: "none",
+              boxShadow: "none",
+              marginTop: "5px",
+            }}
+            onClick={handleLogout}
+            aria-label="Logout"
+          >
+            <span className="d-none d-md-inline">{t("Log Out")}</span>
+            <BoxArrowRight color="#fff" className="fs-6 pe-auto" />
+          </button>
+        </header>
 
-      {totalMovies > 0 ? (
-        <>
-          <Row>
-            {moviesData.map((movie) => (
-              <Col
-                key={movie.id}
-                lg={3}
-                md={6}
-                sm={6}
-                xs={6}
-                onClick={() => handlePosterClick(movie)}
-              >
-                <MovieCard
-                  title={movie.title}
-                  year={movie.publishing_year}
-                  imageSrc={movie.poster}
-                />
-              </Col>
-            ))}
-          </Row>
+        {totalMovies > 0 ? (
+          <>
+            <section aria-label="Movie List">
+              <Row>
+                {moviesData.map((movie) => (
+                  <Col
+                    key={movie.id}
+                    lg={3}
+                    md={6}
+                    sm={6}
+                    xs={6}
+                    onClick={() => handlePosterClick(movie)}
+                  >
+                    <MovieCard
+                      title={movie.title}
+                      year={movie.publishing_year}
+                      imageSrc={movie.poster}
+                    />
+                  </Col>
+                ))}
+              </Row>
+            </section>
 
-          <div className="pagination-wrapper app-bg-color">
-            <PaginationComponent
-              currentPage={currentPage}
-              totalPages={totalPages}
-              onPageChange={handlePageChange}
-            />
-          </div>
-        </>
-      ) : (
-        <EmptyMovieList />
-      )}
-    </Container>
+            <footer className="pagination-wrapper app-bg-color">
+              <PaginationComponent
+                currentPage={currentPage}
+                totalPages={totalPages}
+                onPageChange={handlePageChange}
+              />
+            </footer>
+          </>
+        ) : (
+          <EmptyMovieList />
+        )}
+      </Container>
+    </main>
   );
 };
 
